b516c90203c8c2a7d443a85bab2d75989d8fe6a8
Fabian Mettler <fabian@mettler.cc>
2022-04-02 17:22:58 +0200

    FIX: Formatting

--- AudioMerge/Services/AudioMerger.swift
+++ AudioMerge/Services/AudioMerger.swift
@@ -14,25 +14,25 @@ struct MergeError: Error {
 
 func mergeAudioFiles(audioFiles: [AudioFile], outputURL: URL, completionHandler: @escaping (MergeError?) -> Void) {
     let composition = AVMutableComposition()
-      
+    
     for i in 0 ..< audioFiles.count {
         let compositionAudioTrack :AVMutableCompositionTrack = composition.addMutableTrack(withMediaType: AVMediaType.audio, preferredTrackID: CMPersistentTrackID())!
-                   
+        
         let asset = audioFiles[i].asset
-                   
+        
         let trackContainer = asset.tracks(withMediaType: AVMediaType.audio)
-
         guard trackContainer.count > 0 else {
-           print("nothing")
-           return
+            print("AUDIO_MERGE -> nothing to do...")
+            completionHandler(nil)
+            return
         }
-
-        let audioTrack = trackContainer[0   ]
+        
+        let audioTrack = trackContainer[0]
         let timeRange = CMTimeRange(start: CMTimeMake(value: 0, timescale:  600), duration: audioTrack.timeRange.duration)
-
+        
         try! compositionAudioTrack.insertTimeRange(timeRange, of: audioTrack, at: composition.duration)
     }
-
+    
     let assetExport = AVAssetExportSession(asset: composition, presetName: AVAssetExportPresetAppleM4A)
     
     if assetExport != nil {
@@ -42,27 +42,27 @@ func mergeAudioFiles(audioFiles: [AudioFile], outputURL: URL, completionHandler:
         assetExport!.exportAsynchronously {
             switch assetExport!.status
             {
-                case AVAssetExportSession.Status.failed:
-                    print("AUDIO_MERGE -> failed \(String(describing: assetExport!.error!))")
-                    
-                    if let error = assetExport!.error as? NSError, let message = error.localizedRecoverySuggestion {
-                        completionHandler(MergeError(description: message))
-                    } else {
-                        completionHandler(MergeError(description: assetExport!.error!.localizedDescription))
-                    }
-                case AVAssetExportSession.Status.cancelled:
-                    print("AUDIO_MERGE -> cancelled \(String(describing: assetExport!.error))")
-                case AVAssetExportSession.Status.unknown:
-                    print("AUDIO_MERGE -> unknown\(String(describing: assetExport!.error))")
-                case AVAssetExportSession.Status.waiting:
-                    print("AUDIO_MERGE -> waiting\(String(describing: assetExport!.error))")
-                case AVAssetExportSession.Status.exporting:
-                    print("AUDIO_MERGE -> exporting\(String(describing: assetExport!.error) )")
-                default:
-                    print("Audio Concatenation Complete")
-                    completionHandler(nil)
-                  }
+            case AVAssetExportSession.Status.failed:
+                print("AUDIO_MERGE -> failed \(String(describing: assetExport!.error!))")
+                
+                if let error = assetExport!.error as? NSError, let message = error.localizedRecoverySuggestion {
+                    completionHandler(MergeError(description: message))
+                } else {
+                    completionHandler(MergeError(description: assetExport!.error!.localizedDescription))
+                }
+            case AVAssetExportSession.Status.cancelled:
+                print("AUDIO_MERGE -> cancelled \(String(describing: assetExport!.error))")
+            case AVAssetExportSession.Status.unknown:
+                print("AUDIO_MERGE -> unknown\(String(describing: assetExport!.error))")
+            case AVAssetExportSession.Status.waiting:
+                print("AUDIO_MERGE -> waiting\(String(describing: assetExport!.error))")
+            case AVAssetExportSession.Status.exporting:
+                print("AUDIO_MERGE -> exporting\(String(describing: assetExport!.error) )")
+            default:
+                print("Audio Concatenation Complete")
+                completionHandler(nil)
             }
+        }
     } else {
         let errorMessage = "Could not create AVAssetExportSession"
         print("AVASSET_EXPORT -> failed \(errorMessage)")
